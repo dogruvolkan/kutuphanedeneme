fd56b27e1e5823ea7aac1b08b8c4f8bb34e81d8f
volkan <volkandogru11@gmail.com>
2022-07-22 15:01:49 +0300

    paket deneme

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-import { ExampleComponent } from 'kutuphanedeneme'
+import { ExampleComponent, Button } from 'kutuphanedeneme'
 import 'kutuphanedeneme/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <ExampleComponent text='Merhaba' />
+      <Button text='tıkla' />
+    </>
+  )
 }
 
 export default App
